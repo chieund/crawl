cff6bd185d9ce8ea372597751579b0bd3a70dc5d
chieund <chieund.fat@gmail.com>
2022-07-20 23:48:12 +0700

    add_pagination: add pagin home

--- models/tag.go
+++ models/tag.go
@@ -1,6 +1,9 @@
 package models
 
-import "gorm.io/gorm"
+import (
+	"gorm.io/gorm"
+	"math/rand"
+)
 
 type Tag struct {
 	Id    int `gorm: "primary_key"`
@@ -12,4 +15,11 @@ type Tag struct {
 
 func (tag *Tag) TableName() string {
 	return "tags"
+}
+
+func (tag *Tag) ShowColor() string {
+	in := []string{"badge-primary", "secondary", "success", "danger", "warning", "info", "light", "dark"}
+	randomIndex := rand.Intn(len(in))
+	pick := in[randomIndex]
+	return pick
 }
