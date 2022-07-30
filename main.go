4a8bd69cf0f463364f503f6fb7551b709ecf1731
chieund <chieund.fat@gmail.com>
2022-07-31 00:22:59 +0700

    add_page_detail: add detail article rediect

--- main.go
+++ main.go
@@ -35,5 +35,5 @@ func main() {
 	r.GET("/t/:tag", controller.GetArticleByTag(db))
 	r.GET("/:slug", controller.GetArticleBySlug(db))
 
-	r.Run() // listen and serve on 0.0.0.0:8080 (for windows "localhost:8080")
+	r.Run(":80") // listen and serve on 0.0.0.0:8080 (for windows "localhost:8080")
 }
