56ff158a0f5fdf994903f0b36d07eb01a289b1c3
chieund <c_nguyen@lab.allabout.co.jp>
2022-09-07 13:21:34 +0700

    add_typesense: fix many open file

--- cmd/cronjob/export_json.go
+++ cmd/cronjob/export_json.go
@@ -49,16 +49,17 @@ var ExportJsonCmd = &cobra.Command{
 		cwd, _ := os.Getwd()
 		filePath := path.Join(cwd, filePathJson)
 
+		f, err := os.OpenFile(filePath, os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
+		if err != nil {
+			log.Println(err)
+		}
 		for _, article := range articleResponses {
 			articles1, _ := json.Marshal(article)
-			f, err := os.OpenFile(filePath, os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
-			if err != nil {
-				log.Println(err)
-			}
-			defer f.Close()
 			if _, err := f.WriteString(string(articles1) + "\n"); err != nil {
 				log.Println(err)
 			}
 		}
+
+		defer f.Close()
 	},
 }
